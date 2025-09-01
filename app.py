d0b48cdb7557a32953c7aacdba30c58398770277
rafliad <radenistri@gmail.com>
2025-09-01 11:48:24 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+from sklearn.naive_bayes import GaussianNB  # tambahin ini
+
+# load model
+model = joblib.load("model.pkl")
+
+# request body schema
+class InputData(BaseModel):
+    gula_darah: float
+    tekanan_darah: float
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    """Endpoint utama untuk memeriksa apakah API berjalan."""
+    return {"message": "Selamat Datang di API Prediksi Diabetes. Gunakan endpoint /docs untuk melihat dokumentasi."}
+
+@app.post("/predict")
+def predict(data: InputData):
+    X = [[data.gula_darah, data.tekanan_darah]]
+    prediction = model.predict(X)[0]
+    return {"prediction": int(prediction)}
